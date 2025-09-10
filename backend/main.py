e2f12a0afef32fd27bda7bc5ea5f70fd49ab2324
Atharva <147217989+atharvanev@users.noreply.github.com>
2025-09-09 23:04:14 -0700

    writing FAST api for model

---
+++ backend/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import numpy as np
+from tensorflow import keras
+from PIL import Image
+import io
+
+
+model = keras.models.load_model('./models/best_garbage.keras()')
+
+app = FastAPI()
+
+#add this so any domain can access the api
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = keras.models.load_model('./models/best_garbage(0.95).keras')
+
+IMG_SIZE = (224, 224)
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    image = image.resize(IMG_SIZE)
+    image_array = np.array(image) / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
+
+    predictions = model.predict(image_array)
+    predicted_class = np.argmax(predictions, axis=1)[0] 
+    return {
+        "predicted_class": int(predicted_class),
+        "confidence": float(np.max(predictions))
+    }
